60bfdd4eb2f358b89d1489eb3eae11c9d8c319aa
AlignedMods <aligneddoesstuff@gmail.com>
2024-05-29 21:13:28 +0200

    drew the paddles and ball to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <raylib.h>
 
 int main() {
-  std::cout << "Hello World!";
+
+    const int width = 1200;
+    const int height = 900;
+    int fps = 60;
+
+    InitWindow(width,height,"game");
+    SetTargetFPS(fps);
+
+    while(WindowShouldClose() == !true)
+    {
+        BeginDrawing();
+
+        DrawLine(width/2,0,width/2,height,WHITE);
+        DrawCircle(width/2,height/2,20,WHITE);
+        DrawRectangle(0,height/2-60,25,120,WHITE);
+        DrawRectangle(width-25,height/2-60,25,120,WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
   return 0;
 }
